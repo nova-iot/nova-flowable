3b143b76c53fc043efafe3ea6bf00820978bfa5b
medy <1228022817@qq.com>
2024-12-12 20:46:51 +0800

    add sidebar button

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -5,11 +5,18 @@ import {
     SidebarGroup,
     SidebarHeader,
   } from "@/components/ui/sidebar"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
   
   export function AppSidebar() {
     return (
       <Sidebar>
-        <SidebarHeader />
+        <SidebarHeader >
+        <div className="flex w-full max-w-sm items-center space-x-1">
+          <Input placeholder="Email" />
+          <Button type="submit">搜索</Button>
+        </div>
+        </SidebarHeader>
         <SidebarContent>
           <SidebarGroup />
           <SidebarGroup />
